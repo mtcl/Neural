7d6bb122bb00ce9acb417f118bca4c47ca462f2f
Mukul Tripathi <texasdude11@gmail.com>
2014-07-24 04:06:41 -0500

    Update NeuralNetworks.groovy
    Added Error Function for each node.

--- NeuralNetworks.groovy
+++ NeuralNetworks.groovy
@@ -17,7 +17,16 @@ def yzSize = [inputParamsCount, midLayerMaxElementsCount, outputParamsCount].max
 def w = new double[xSize][yzSize][yzSize]
 
 //define intermediate output matrix
-def out = new double[xSize+1][yzSize]
+def out = new double[xSize + 1][yzSize]
+
+//define error
+def err = new double[xSize + 1][yzSize]
+
+//initialize first set of inputs
+def x = [0.05, 0.02]
+
+//initialize first set of outputs
+def y = [1, 0]
 
 //display neural network weights
 def displayMatrix = { x1 ->
@@ -44,36 +53,73 @@ for (int i = 0; i < xSize; i++) {
         }
     }
 }
+
+w=[[[0.5, 0.4, 0.1], [0.2, 0.6, 0.2], [0,0,0]], [[0.10, 0.55, 0.35], [0.2, 0.45, 0.35], [0.25, 0.15, 0.60]], [[0.3, 0.35, 0], [0.35, 0.25, 0], [0.45, 0.3,0]]]
 displayMatrix(w)
 
-//initialize first set of inputs
-def x = [0.05, 0.02]
-
-//initialize first element of output matrix
+//initialize first element of out matrix
 (0..x.size() - 1).each {
     out[0][it] = x[it]
 }
+
+//Initializing remaining elements of out matrix
 def sum
-for (int i = 1; i < xSize+1; i++) {
-    //for input parameters
+for (int i = 1; i < xSize + 1; i++) {
+
+    // find the maximum number of elements in the next layer
+    //for input parameters i is inputParamsCount
     kmax = i == 1 ? inputParamsCount : midLayerMaxElementsCount
     //for output parameters
     yzSize1 = i == xSize ? outputParamsCount : yzSize
 
     for (int j = 0; j < yzSize1; j++) {
-        sum=0
-        for (int k=0; k<kmax; k++){
-            sum+= w[i-1][k][j]*out[i-1][k]
-            //println('w['+(i-1)+']['+k+']['+j+']*out['+(i-1)+']['+k+']')
+        sum = 0
+        for (int k = 0; k < kmax; k++) {
+            sum += w[i - 1][k][j] * out[i - 1][k]
+            //print('w['+(i-1)+']['+k+']['+j+']*out['+(i-1)+']['+k+']  + ')
+            //print(w[i - 1][k][j] + ' * ' + out[i - 1][k] + ' + ')
         }
-       //println()
-        out[i][j]=fSigmoid(sum)
+        out[i][j] = fSigmoid(sum)
+        //print(' = ' + out[i][j])
+        //println()
     }
 }
 
-for (int i = 0; i < xSize+1; i++) {
+for (int i = 0; i < xSize + 1; i++) {
     for (int j = 0; j < yzSize; j++) {
-        print out[i][j]+ " "
+        print out[i][j] + " "
     }
+    println()
+}
+
+//initialize first element of error matrix
+(0..y.size() - 1).each {
+    err[xSize][it] = y[it]-out[xSize][it]
+}
 println()
+//Initializing remaining elements of err matrix
+//looping backwards in the error matrix (since the length of array is xSize+1, we will start with xSize-1
+//the intent is to start with last inner layer in the neural network
+for (int i = xSize-1; i >= 1; i--) {
+
+    //find the maximum number of elements in the next layer
+    kmax = i == xSize-1 ? outputParamsCount : midLayerMaxElementsCount
+
+    //looping over all the elements in the current layer.
+    for (int j = 0; j < yzSize; j++) {
+        sum=0
+        for (int k = 0; k < kmax; k++) {
+            sum+= w[i][j][k]*err[i+1][k]
+            //println('w['+(i)+']['+j+']['+k+']*err['+(i+1)+']['+k+']')
+        }
+        //println()
+        err[i][j] = out[i][j]*(1-out[i][j])*sum
+    }
+}
+
+for (int i = 0; i < xSize + 1; i++) {
+    for (int j = 0; j < yzSize; j++) {
+        print err[i][j] + " "
+    }
+    println()
 }
