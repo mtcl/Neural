0644e3d8509fea479d1bbf98e84ca94cba215f75
Mukul Tripathi <texasdude11@gmail.com>
2014-07-27 02:38:46 -0500

    Added backpropagation method to facilitate looping. Currently the only return parameter from this methos is the w - weights.

--- NeuralNetworks.groovy
+++ NeuralNetworks.groovy
@@ -60,88 +60,99 @@ for (int i = 0; i < xSize; i++) {
 w = [[[0.5, 0.4, 0.1], [0.2, 0.6, 0.2], [0, 0, 0]], [[0.10, 0.55, 0.35], [0.2, 0.45, 0.35], [0.25, 0.15, 0.60]], [[0.3, 0.35, 0], [0.35, 0.25, 0], [0.45, 0.3, 0]]]
 displayMatrix(w)
 
-//initialize first element of out matrix
-(0..x.size() - 1).each {
-    out[0][it] = x[it]
-}
+def backpropagation(fSigmoid, inputParamsCount, midLayerMaxElementsCount, outputParamsCount, innerLayer, alpha, xSize, yzSize, w, out, err, x, y) {
 
-//Initializing remaining elements of out matrix
-def sum
-for (int i = 1; i < xSize + 1; i++) {
-
-    // find the maximum number of elements in the next layer
-    //for input parameters i is inputParamsCount
-    kmax = i == 1 ? inputParamsCount : midLayerMaxElementsCount
-    //for output parameters
-    yzSize1 = i == xSize ? outputParamsCount : yzSize
-
-    for (int j = 0; j < yzSize1; j++) {
-        sum = 0
-        for (int k = 0; k < kmax; k++) {
-            sum += w[i - 1][k][j] * out[i - 1][k]
-            //print('w['+(i-1)+']['+k+']['+j+']*out['+(i-1)+']['+k+']  + ')
-            //print(w[i - 1][k][j] + ' * ' + out[i - 1][k] + ' + ')
-        }
-        out[i][j] = fSigmoid(sum)
-        //print(' = ' + out[i][j])
-        //println()
+    //Start out matrix calculation
+    //initialize first element of out matrix
+    (0..x.size() - 1).each {
+        out[0][it] = x[it]
     }
-}
 
-for (int i = 0; i < xSize + 1; i++) {
-    for (int j = 0; j < yzSize; j++) {
-        print out[i][j] + " "
+    //Initializing remaining elements of out matrix
+    def sum
+    for (int i = 1; i < xSize + 1; i++) {
+
+        //The last row of the out elements is the final output
+
+        // find the maximum number of elements in the next layer
+        //for input parameters i is inputParamsCount
+        kmax = i == 1 ? inputParamsCount : midLayerMaxElementsCount
+        //for output parameters
+        yzSize1 = i == xSize ? outputParamsCount : yzSize
+
+        for (int j = 0; j < yzSize1; j++) {
+            sum = 0
+            for (int k = 0; k < kmax; k++) {
+                sum += w[i - 1][k][j] * out[i - 1][k]
+                //print('w['+(i-1)+']['+k+']['+j+']*out['+(i-1)+']['+k+']  + ')
+                //print(w[i - 1][k][j] + ' * ' + out[i - 1][k] + ' + ')
+            }
+            out[i][j] = fSigmoid(sum)
+            //print(' = ' + out[i][j])
+            //println()
+        }
+    }
+    /*
+    for (int i = 0; i < xSize + 1; i++) {
+        for (int j = 0; j < yzSize; j++) {
+            print out[i][j] + " "
+        }
+        println()
+    }
+    */
+    //end out matrix calculation
+
+    //initialize first element of error matrix
+    (0..y.size() - 1).each {
+        err[xSize][it] = y[it] - out[xSize][it]
     }
     println()
-}
+    //Initializing remaining elements of err matrix
+    //looping backwards in the error matrix (since the length of array is xSize+1, we will start with xSize-1
+    //the intent is to start with last inner layer in the neural network
+    for (int i = xSize - 1; i >= 1; i--) {
 
-//initialize first element of error matrix
-(0..y.size() - 1).each {
-    err[xSize][it] = y[it] - out[xSize][it]
-}
-println()
-//Initializing remaining elements of err matrix
-//looping backwards in the error matrix (since the length of array is xSize+1, we will start with xSize-1
-//the intent is to start with last inner layer in the neural network
-for (int i = xSize - 1; i >= 1; i--) {
+        //find the maximum number of elements in the next layer
+        kmax = i == xSize - 1 ? outputParamsCount : midLayerMaxElementsCount
 
-    //find the maximum number of elements in the next layer
-    kmax = i == xSize - 1 ? outputParamsCount : midLayerMaxElementsCount
-
-    //looping over all the elements in the current layer.
-    for (int j = 0; j < yzSize; j++) {
-        sum = 0
-        for (int k = 0; k < kmax; k++) {
-            sum += w[i][j][k] * err[i + 1][k]
-            //println('w['+(i)+']['+j+']['+k+']*err['+(i+1)+']['+k+']')
+        //looping over all the elements in the current layer.
+        for (int j = 0; j < yzSize; j++) {
+            sum = 0
+            for (int k = 0; k < kmax; k++) {
+                sum += w[i][j][k] * err[i + 1][k]
+                //println('w['+(i)+']['+j+']['+k+']*err['+(i+1)+']['+k+']')
+            }
+            //println()
+            err[i][j] = out[i][j] * (1 - out[i][j]) * sum
         }
-        //println()
-        err[i][j] = out[i][j] * (1 - out[i][j]) * sum
     }
-}
 
-for (int i = 0; i < xSize + 1; i++) {
-    for (int j = 0; j < yzSize; j++) {
-        print err[i][j] + " "
+    for (int i = 0; i < xSize + 1; i++) {
+        for (int j = 0; j < yzSize; j++) {
+            print err[i][j] + " "
+        }
+        println()
     }
-    println()
-}
-println('adjusting weights')
-//Adjust weights for each layer
-for (int i = 0; i < xSize; i++) {
-    for (int j = 0; j < yzSize; j++) {
-        for (int k = 0; k < yzSize; k++) {
+    println('adjusting weights')
+    //Adjust weights for each layer
+    for (int i = 0; i < xSize; i++) {
+        for (int j = 0; j < yzSize; j++) {
+            for (int k = 0; k < yzSize; k++) {
 
-            if ((i == 0 && j > inputParamsCount - 1) || (i == innerLayer && k >= outputParamsCount)) {
-                w[i][j][k] = 0
-            } else {
-                //print('w'+ '[' + i + ',' + j + ',' + k + '] ->> ')
-                //println(w[i][j][k] + ' + ' + alpha +  '*' + err[i + 1][k] + ' * ' + out[i][k] + ' = ' + w[i][j][k])
-                w[i][j][k] = w[i][j][k] + alpha * err[i + 1][k] * out[i][k]
+                if ((i == 0 && j > inputParamsCount - 1) || (i == innerLayer && k >= outputParamsCount)) {
+                    w[i][j][k] = 0
+                } else {
+                    //print('w'+ '[' + i + ',' + j + ',' + k + '] ->> ')
+                    //println(w[i][j][k] + ' + ' + alpha +  '*' + err[i + 1][k] + ' * ' + out[i][k] + ' = ' + w[i][j][k])
+                    w[i][j][k] = w[i][j][k] + alpha * err[i + 1][k] * out[i][k]
 
+                }
             }
         }
     }
+    println('adjusted weights')
+    return [w]
 }
-println('adjusted weights')
-displayMatrix(w)
+
+
+w = backpropagation(fSigmoid, inputParamsCount, midLayerMaxElementsCount, outputParamsCount, innerLayer, alpha, xSize, yzSize, w, out, err, x, y)
